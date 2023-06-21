fad0675080d2af2b22e972839ecd6aca5a89198e
Ishaipita <108309123+Ishaipita@users.noreply.github.com>
2023-06-21 09:49:10 +0300

    Delete build_test.sh

--- build_test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-set -o pipefail
-
-sudo chmod -R 777 /github/
-pip install --user --upgrade pip==18.0
-pip install --user --no-cache-dir -r requirements.txt
-python test_hello_world.py
